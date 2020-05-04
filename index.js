27d3263a32773c2d4dd6eb8c7cc4186bfece3a2a
Ricardo Sacardo <rperezsacardo@gmail.com>
2020-05-04 14:44:00 +0100

    Done

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
 class SortedList {
   constructor() {
+    this.items = [];
+    this.length = this.items.length
     // ...
   }
 
   add(item) {
-    // ...
-  }
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length 
+ }
 
   load(pos) {
+    if(pos > this.length - 1){
+      throw new Error('OutOfBounds');
+    }else{
+      return this.items[pos]
+    }
+
     // ...
   }
 
   max() {
+    if(!this.length){
+      throw new Error('EmptySortedList');
+    }else{
+     return Math.max(...this.items)
+    }
+
     // ...
   }
 
   min() {
+    if(!this.length){
+      throw new Error('EmptySortedList');
+    }else{
+     return Math.min(...this.items)
+    }
+
     // ...
+  
   }
 
-  sum() {
-    // ...
+  sum() {    return this.items.reduce((acc, i) => acc + i, 0)
+    
   }
 
   avg() {
-    // ...
+
+    if(!this.length){
+      throw new Error('EmptySortedList');
+    }else{
+    return (this.items.reduce((acc, i) => acc + i, 0))/ this.length
+    // return (this.items.sum())/ this.length // Fail at the test
+    }
+    
   }
 }
 
